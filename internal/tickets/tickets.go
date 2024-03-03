cc0d2ac93f0a525463a3ef144c8d53c979c54ab9
flor-muller <mariaflorenciamuller@gmail.com>
2024-03-03 18:34:29 -0300

    finalizado

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -15,9 +15,6 @@ const (
 	noche     = "noche"
 )
 
-// id, nombre, email, país de destino, hora del vuelo y precio.
-// 1,Tait Mc Caughan,tmc0@scribd.com,Finland,17:11,785
-
 type Ticket struct {
 	Id          int
 	Name        string
@@ -27,70 +24,77 @@ type Ticket struct {
 	Price       float64
 }
 
-// Funcion lectura archivo
-func GetFileData() []string {
+// Slice con total de tickets diarios
+var TicketsAll []Ticket
+
+// Funcion lectura archivo, almacenado de informacion y retorno de total
+func GetFileData() int {
+
 	fileName := "./tickets.csv"
+	//Manejo panic
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	//Lectura archivo
 	res, err := os.ReadFile(fileName)
 	if err != nil {
-		fmt.Println("Error al intentar leer el archivo: ", fileName)
+		panic("\n ALERTA! El archivo indicado no fue encontrado o está dañado: " + fileName + " No podran realizarse los calculos.\n")
 	}
+	//Extraccion de datos y almacenamiento en slice de estructuras Ticket
 	data := strings.Split(string(res), "\n")
-	return data
-}
-
-// Funcion Calculo total de tickets diarios (todos los destinos)
-func GetTotal() {
-
-}
-
-// ejemplo 1: Funcion calculo total de tickets por destino
-func GetTotalTickets(destination string) (int, error) {
-	data := GetFileData()
-	var total int
 	for i := 0; i < len(data)-1; i++ {
-		ticket := strings.Split(string(data[i]), ",")
-		if ticket[3] == destination {
-			total += 1
-		}
-	}
-	return total, nil
-}
-
-// ejemplo 2
-func CountByPeriod(min int64, max int64) (int, error) {
-	data := GetFileData()
-	var total int
-	for i := 0; i < len(data)-1; i++ {
-		ticket := strings.Split(string(data[i]), ",")
-		hourString := strings.Split(string(ticket[4]), ":")
-		hour, err := strconv.ParseInt(hourString[0], 10, 64)
+		ticketData := strings.Split(string(data[i]), ",")
+		id, err := strconv.Atoi(ticketData[0])
 		if err != nil {
-			fmt.Println("Error al intentar parsear la hora.")
+			fmt.Printf("Error al intentar parsear el id del ticket con email: %s Revisar ticket \n", ticketData[2])
 		}
-		if hour >= min && hour <= max {
+		price, err := strconv.ParseFloat(ticketData[5], 64)
+		if err != nil {
+			fmt.Printf("Error al intentar parsear el precio del ticket con email: %s Revisar ticket \n", ticketData[2])
+		}
+		ticket := Ticket{
+			Id:          id,
+			Name:        ticketData[1],
+			Email:       ticketData[2],
+			Destination: ticketData[3],
+			Hour:        ticketData[4],
+			Price:       price,
+		}
+		TicketsAll = append(TicketsAll, ticket)
+	}
+	totalTickets := len(TicketsAll)
+	return totalTickets
+}
+
+// Funcion calculo total de tickets por destino
+func GetTotalTickets(destination string) (int, error) {
+	var total int
+	for _, ticket := range TicketsAll {
+		if ticket.Destination == destination {
 			total += 1
 		}
+	}
+	if total == 0 {
+		return 0, errors.New("No hay pasajes al destino indicado o El destino indicado es invalido.")
 	}
 	return total, nil
 }
 
-// ejemplo 2 enunciado
+// Funcion derivadora para calculo tickets por horario vuelo
 func GetCountByPeriod(time string) (int, error) {
 	switch time {
 	case madrugada:
-		//0 a 6
 		total, err := CountByPeriod(0, 6)
 		return total, err
 	case manana:
-		//7 a 12
 		total, err := CountByPeriod(7, 12)
 		return total, err
 	case tarde:
-		//13 a 19
 		total, err := CountByPeriod(13, 19)
 		return total, err
 	case noche:
-		//20 a 23
 		total, err := CountByPeriod(20, 23)
 		return total, err
 	default:
@@ -99,7 +103,33 @@ func GetCountByPeriod(time string) (int, error) {
 
 }
 
-// ejemplo 3: Funcion calculo porcentaje tickets destino respecto a total tickets
+// Funcion calculo tickets por horario vuelo
+func CountByPeriod(min int, max int) (int, error) {
+	var total int
+	for _, ticket := range TicketsAll {
+		hourString := strings.Split(string(ticket.Hour), ":")
+		hour, err := strconv.Atoi(hourString[0])
+		if err != nil {
+			return 0, errors.New("Error al intentar parsear la hora del ticket con email: " + ticket.Email + ". Revisar ticket")
+		}
+		if hour >= min && hour <= max {
+			total += 1
+		}
+	}
+	return total, nil
+}
+
+// Funcion calculo porcentaje tickets destino respecto a total tickets
+// (Mantuve la firma indicada en el enunciado, sin embargo no seria necesario recibir el total de tickets por parametro.
+// Se podria acceder directo a length de TotalTickets dentro de esta funcion)
 func AverageDestination(destination string, total int) (float64, error) {
-	return 1, nil
+	totalDestination, err := GetTotalTickets(destination)
+	if err != nil {
+		return 0, err
+	}
+	if total == 0 {
+		return 0, errors.New("Error en el numero de pasajes totales. Valor: 0")
+	}
+	percentage := float64(totalDestination) / float64(total) * 100
+	return percentage, nil
 }
--- main.go
+++ main.go
@@ -7,23 +7,41 @@ import (
 
 func main() {
 
-	//Manejo de errores y panic
-	//goroutines
-	//estructuras
-	//revisar error de CountByPeriod
+	destination := "Brazil"
+	hour := "noche"
 
-	//total, err := tickets.GetTotalTickets("Brazil")
-	//arrayData := tickets.GetFileData()
-	//fmt.Println(arrayData[0])
-	destination := "Japan"
-	total, err := tickets.GetTotalTickets(destination)
-	if err != nil {
-		fmt.Printf("Error al calcular el total de tickets por destino.")
-	}
-	fmt.Printf("Total de tickets para %s es %d \n", destination, total)
-	totalPeriod, err := tickets.GetCountByPeriod("manana")
-	if err != nil {
-		fmt.Printf("Error al calcular el total de tickets por periodo.")
-	}
-	fmt.Printf("Total de tickets para manana es %d \n", totalPeriod)
+	totalTickets := tickets.GetFileData()
+	fmt.Println("El total de tickets vendidos del dia es:", totalTickets)
+
+	destintationCh := make(chan int)
+	hourCh := make(chan int)
+	percentageCh := make(chan float64)
+
+	go func(destination string) {
+		totalDestination, err := tickets.GetTotalTickets(destination)
+		if err != nil {
+			fmt.Println("El siguiente ERROR ha ocurrido al calcular el total de tickets por destino.", err)
+		}
+		destintationCh <- totalDestination
+	}(destination)
+
+	go func(hour string) {
+		totalPeriod, err := tickets.GetCountByPeriod(hour)
+		if err != nil {
+			fmt.Println("El siguiente ERROR ha ocurrido al calcular el total de tickets por periodo:", err)
+		}
+		hourCh <- totalPeriod
+	}(hour)
+
+	go func(destination string) {
+		percentage, err := tickets.AverageDestination(destination, totalTickets)
+		if err != nil {
+			fmt.Println("El siguiente ERROR ha ocurrido al calcular el porcentaje de tickets por destino:", err)
+		}
+		percentageCh <- percentage
+	}(destination)
+
+	fmt.Printf("El total de tickets vendidos con destino a %s es: %d \n", destination, <-destintationCh)
+	fmt.Printf("El total de tickets vendidos con hora de vuelo en el turno de %s es: %d \n", hour, <-hourCh)
+	fmt.Printf("El porcentaje de tickets vendidos con destino a %s es de: %.1f%% \n", destination, <-percentageCh)
 }
